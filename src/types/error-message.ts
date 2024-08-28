bcb953a20bb68173f0e4a5b6709fde6ba9a4a395
Hamed <hamedsedighi@gmail.com>
2024-08-28 08:12:52 +0200

    fix typo

--- src/types/error-message.ts
+++ src/types/error-message.ts
@@ -1,6 +1,6 @@
 export enum ErrorMessage {
   USER_NOT_FOUND = 'Requested user was not found',
   FAILED_TO_FETCH_BRANCHES = 'Failed to fetch branches',
-  USER_HAS_NO_REPOSITORY = 'User has no reposity',
+  USER_HAS_NO_REPOSITORY = 'User has no repository',
   FAILED_TO_FETCH_COMMITS = 'Failed to fetch commits',
 }
